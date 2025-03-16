a7b2ab8592bef841a97ebb86bb72340eefe46dd2
DepartureZSH <857633459@qq.com>
2025-03-16 17:55:38 +0800

    Update Blip2

--- src/smart/extractor/Caption_Blip2.py
+++ src/smart/extractor/Caption_Blip2.py
@@ -10,15 +10,14 @@ import torch
 from PIL import Image
 import numpy
 
-
 devices = []
 if torch.cuda.is_available():
     num_gpus = torch.cuda.device_count()
     devices = [torch.device(f'cuda:{i}') for i in range(num_gpus)]
 else:
     devices = [torch.device('cpu')]
-device = devices[0]
-splits = ["val", "test"]
+device = devices[1]
+splits = ["train"]
 
 def read_tsv(file):
     return pd.read_csv(file, sep='\t', header=None)
@@ -30,7 +29,7 @@ def read_json(file):
 def write_csv(data, dest_file):
     data.to_csv(dest_file, sep='\t', quoting=3, header=False, index=False)
 
-def Blip2_Caption_globally(model, processor):
+def Blip2_Caption_globally(model, processor, prompt=None):
     for split in splits:
         data = read_tsv(f"{data_folder}/obj_feat_{split}.tsv")
         image_mapping = read_json(f"{data_folder}/Features/idx_image_id_mapping.json")
@@ -39,43 +38,48 @@ def Blip2_Caption_globally(model, processor):
             objects_list = []
             indx = data.iloc[ind, 0]
             image_path = image_mapping[str(indx)]
-            objects = json.loads(data.iloc[ind, 1])
-            image = Image.open(f"{image_folder}/{image_path}").convert('RGB') # PIL Image
-            prompt = "Question: What is the relationship between the main objects in the image? Answer:"
-            inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
+            image = Image.open(f"{data_folder}/{image_path}").convert('RGB') # PIL Image
+            # prompt = "Question: What is the relationship between the main objects in the image? Answer:"
+            if prompt is None:
+                inputs = processor(images=image, return_tensors="pt").to(device)
+            else:
+                inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
             generated_ids = model.generate(**inputs, max_new_tokens=20)
             caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
             caption = caption.split("Answer:")[-1]
             objects_list.append({"G_caption": caption})
             data.iloc[ind, 1] = json.dumps({"objects": objects_list}, ensure_ascii=False)
-        write_csv(data, f"{data_folder}/Features/BLIP2/Global/obj_feat_{split}.tsv")
+        if prompt is None:
+            write_csv(data, f"{data_folder}/Features/BLIP2/Global/obj_feat_{split}.tsv")
+        else:
+            write_csv(data, f"{data_folder}/Features/BLIP2/Global_Prompt/obj_feat_{split}_prompt.tsv")
 
-def Blip2_Caption_locally(model, processor):
+def Blip2_Caption_locally(model, processor, prompt=None):
     for split in splits:
-        data = read_tsv(f"{data_folder}/Features/obj_feat_{split}.tsv")
+        data = read_tsv(f"{data_folder}/obj_feat_{split}.tsv")
         image_mapping = read_json(f"{data_folder}/Features/idx_image_id_mapping.json")
-        prompt = "Question: What is the relationship between the main objects in the image? Answer:"
+        # prompt = "Question: What is the relationship between the main objects in the image? Answer:"
         for ind in range(len(data)):
             print(f"Processing {ind}/{len(data)}")
             objects_list = []
             indx = data.iloc[ind, 0]
             image_path = image_mapping[str(indx)]
             objects = json.loads(data.iloc[ind, 1])
-            image = Image.open(f"{image_folder}/{image_path}").convert('RGB') # PIL Image
+            image = Image.open(f"{data_folder}/{image_path}").convert('RGB') # PIL Image
             for j, obj in enumerate(objects["objects"]):
                 image_patch = image.crop(obj["rect"])
-                inputs = processor(images=image_patch, text=prompt, return_tensors="pt").to(device)
+                if prompt is None:
+                    inputs = processor(images=image_patch, return_tensors="pt").to(device)
+                else:
+                    inputs = processor(images=image_patch, text=prompt, return_tensors="pt").to(device)
                 generated_ids = model.generate(**inputs, max_new_tokens=20)
                 caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
                 objects_list.append({"caption": caption})
             data.iloc[ind, 1] = json.dumps({"objects": objects_list}, ensure_ascii=False)
-        write_csv(data, f"{data_folder}/Features/BLIP2/Local/obj_feat_{split}.tsv")
-
-# def Blip2_Predict(data, model, device, batch_size=64):
-#     model.to(device)
-#     model.eval()
-#     num_batches = len(data) // batch_size
-#     image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+        if prompt is None:
+            write_csv(data, f"{data_folder}/Features/BLIP2/Local/obj_feat_{split}.tsv")
+        else:
+            write_csv(data, f"{data_folder}/Features/BLIP2/Local_Prompt/obj_feat_{split}_prompt.tsv")
 
 def validate(file):
     data = pd.read_csv(file, sep='\t', header=None)
@@ -95,6 +99,7 @@ if __name__ == '__main__':
     )
     
     model.to(device)
+    prompt = "Question: What is the relationship between the main objects in the image? Answer:"
     # Blip2_Caption_globally(model, processor)
     Blip2_Caption_locally(model, processor)
     # validate(f"{data_folder}/Features/obj_feat_val.tsv")
